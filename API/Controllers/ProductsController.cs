970f6c87077df3475326c15ea6b48759b1f851d3
GaneshS <ganesh.sanap2326@gmail.com>
2021-12-17 10:19:46 +0530

    Added sorting in specification. Handled decimal conversion for SQLite

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -34,9 +34,9 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts()
+        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string sort)
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification();
+            var spec = new ProductsWithTypesAndBrandsSpecification(sort);
             
             var products = await _productRepository.ListAsync(spec);
 
