9f9281406956b51cf3ad73bacd2b9eff53d7dded
GaneshS <ganesh.sanap2326@gmail.com>
2021-12-27 21:38:26 +0530

    Adde Basket entities and Repository

--- API/Startup.cs
+++ API/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -27,6 +28,11 @@ namespace API
             services.AddControllers();
             services.AddDbContext<StoreContext>(options => 
                 options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddSingleton<IConnectionMultiplexer>(c =>
+            {
+                var configuration = ConfigurationOptions.Parse(Configuration.GetConnectionString("Redis"), true);
+                return ConnectionMultiplexer.Connect(configuration);
+            });
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
             services.AddCors(opt =>
