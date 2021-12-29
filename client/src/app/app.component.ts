f56704529badf85dece45c628550a39a8c4d5c2b
GaneshS <ganesh.sanap2326@gmail.com>
2021-12-29 20:05:40 +0530

    Maintain logged in user in navbar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 
 @Component({
@@ -9,7 +10,25 @@ import { BasketService } from './basket/basket.service';
 
 export class AppComponent implements OnInit {
   title = 'SkiNet';
-  constructor(private basketService: BasketService) {
+  constructor(private basketService: BasketService, private accountService: AccountService) { }
+
+  ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+    if(token) {
+      this.accountService.loadCurrentUser(token).subscribe(() => {
+        console.log('user loaded');
+      }, error => {
+        console.log(error);
+      });
+    }
+  }
+
+  loadBasket() {
     const basketId = localStorage.getItem('basket_id');
     if (basketId) {
       this.basketService.getBasket(basketId).subscribe(() => {
@@ -18,9 +37,5 @@ export class AppComponent implements OnInit {
         console.log(error);
       });
     }
-  }
-
-  ngOnInit(): void {
-      
-  }
+  }  
 }
