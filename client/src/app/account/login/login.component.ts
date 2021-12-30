94272bfb4250a3adc6c866154ee320a7222f67b0
GaneshS <ganesh.sanap2326@gmail.com>
2021-12-30 11:36:59 +0530

    Added validation for login

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -14,14 +14,15 @@ export class LoginComponent implements OnInit {
   constructor(private accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
-    this.createLoginForm()
+    this.createLoginForm();
   }
 
   createLoginForm() {
     this.loginForm = new FormGroup({
-      email: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators
+        .pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]),
       password: new FormControl('', Validators.required)
-    })
+    });
   }
 
   onSubmit() {
