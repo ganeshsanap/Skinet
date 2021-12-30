eac0e005766d80834d202d3ca61ccfbc59d67b34
GaneshS <ganesh.sanap2326@gmail.com>
2021-12-30 14:08:13 +0530

    Added auth aurd for checkout

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -19,13 +19,12 @@ export class AppComponent implements OnInit {
 
   loadCurrentUser() {
     const token = localStorage.getItem('token');
-    if(token) {
-      this.accountService.loadCurrentUser(token).subscribe(() => {
-        console.log('user loaded');
-      }, error => {
-        console.log(error);
-      });
-    }
+    
+    this.accountService.loadCurrentUser(token).subscribe(() => {
+      console.log('user loaded');
+    }, error => {
+      console.log(error);
+    });
   }
 
   loadBasket() {
